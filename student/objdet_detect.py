6e0be276a10a124d2c0bab21aa6d9c9ab79e985c
Neha Verma <nehbehl@gmail.com>
2021-08-14 22:38:49 +0530

    Update objdet_detect.py

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -61,7 +61,41 @@ def load_configs_model(model_name='darknet', configs=None):
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
-
+        configs.saved_fn = 'fpn_resnet'
+        configs.arch = 'fpn_resnet'
+        configs.pretrained_path = 'tools/objdet_models/resnet/pretrained/fpn_resnet_18_epoch_300.pth'
+        configs.K = 50
+        configs.no_cuda = False
+        configs.gpu_idx = 0
+        configs.batch_size = 4
+        configs.num_samples = None
+        configs.num_workers = 1
+        configs.peak_thresh = 0.2
+        configs.save_test_output = False
+        configs.output_format = 'image'
+        configs.output_video_fn = 'out_fpn_resnet'
+        configs.output_width = 608
+        configs.pin_memory = True
+        configs.distributed = False
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2
+        configs.heads = {
+        'hm_cen': configs.num_classes,
+        'cen_offset': configs.num_center_offset,
+        'direction': configs.num_direction,
+        'z_coor': configs.num_z,
+        'dim': configs.num_dim
+        }
+        configs.num_input_features = 4
         #######
         ####### ID_S3_EX1-3 END #######     
 
@@ -118,7 +152,10 @@ def create_model(configs):
         ####### ID_S3_EX1-4 START #######     
         #######
         print("student task ID_S3_EX1-4")
-
+        num_layers = 20
+        model = fpn_resnet.get_pose_net(num_layers = num_layers, heads = configs.heads, 
+                                        head_conv= configs.head_conv, 
+                                        imagenet_pretrained = configs.imagenet_pretrained)
         #######
         ####### ID_S3_EX1-4 END #######     
     
@@ -168,6 +205,12 @@ def detect_objects(input_bev_maps, model, configs):
             #######
             print("student task ID_S3_EX1-5")
 
+                      # detections size (batch_size, K, 10)
+            detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
+                                outputs['dim'], K=configs.K)
+            detections = detections.cpu().numpy().astype(np.float32)
+            detections = post_processing(detections, configs.num_classes, configs.down_ratio, configs.peak_thresh)  
+            detections = detections[0]
             #######
             ####### ID_S3_EX1-5 END #######     
 
@@ -180,13 +223,19 @@ def detect_objects(input_bev_maps, model, configs):
     objects = [] 
 
     ## step 1 : check whether there are any detections
-
+    for object_found in detections:
+        id, bev_x, bev_y, z, h, bev_w, bev_l, yaw = object_found
         ## step 2 : loop over all detections
-        
+        x = bev_y / configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+        y = bev_x / configs.bev_width * (configs.lim_y[1] - configs.lim_y[0]) - (configs.lim_y[1] - configs.lim_y[0])/2.0 
+        w = bev_w / configs.bev_width * (configs.lim_y[1] - configs.lim_y[0]) 
+        l = bev_l / configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
             ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
-        
+        if ((x >= configs.lim_x[0]) and (x <= configs.lim_x[1])
+            and (y >= configs.lim_y[0]) and (y <= configs.lim_y[1])
+            and (z >= configs.lim_z[0]) and (z <= configs.lim_z[1])):
             ## step 4 : append the current object to the 'objects' array
-        
+            objects.append([1, x, y, z, h, w, l, yaw])
     #######
     ####### ID_S3_EX2 START #######   
     
